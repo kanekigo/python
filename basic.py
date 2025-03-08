49121e4763a343dda824dab23157bc25107af51e
kaneki <gopalrajpurohit2152006@gmail.com>
2025-03-08 14:08:55 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,67 @@
+#python interpreter executes our code line by line from the top
+
+#Variables
+#full_name = 'john smith'
+#age = 20
+#is_new = True
+
+#Receiving Input
+#name = input('what is your name ')
+#color = input('what is your favourite color ')
+#print ( name,'likes',color )
+
+#Type Conversion
+#weight_lbs = input('Weight (lbs): ')
+#weight_kg = int(weight_lbs) * 0.45
+#print(weight_kg)
+
+#There are few more function for converting values into diffrent types.
+#int()    for converting a string into an integer.
+#float()  for converting a string into a float,or a number with a decimal point.
+#bool()   for converting a string into a boolean value.
+
+#strings
+#python string are indexed from 0,1,2,3,4,5,etc.
+#name = 'john smith'
+#print (name[1:-1])
+
+#format string
+#first = 'john'
+#last = 'smith'
+#message = first + '[' + last+ '] is a coder'
+#msg = f'{first} [{last}] is a coder'
+#print (msg)
+
+#string methods
+#course = 'python for Beginners'
+#len()
+#course.upper()
+#course.lower()
+#course.title()
+#course.find()
+#course.find()
+#course.replace()
+#'....' in course
+
+
+#Arithmetic  Operations
+# + (addition)
+# - (subraction)
+# * (multiple)
+# / (division)
+# % (modules)
+# // (floor division) --- display real part only
+
+#we called this one a augmented assingment operator
+#x = 10
+#----x += 3---
+#print(x)
+
+#Operator Precedence
+#x = 10 + 3 * 2 ** 2
+#exponentiation 2 ** 3
+#multiplication or division
+#addition or subtraction
+
+#parenthesis
+#x = (10 + 3) * 2 ** 2
